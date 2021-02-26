2673c6e089627aeb7640073df08306c6743dc33a
unknown <tlin41@dons.usfca.edu>
2021-02-26 15:16:21 -0800

    created multiple mapping points.

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,11 +1,6 @@
 //Add console.log to check to see if our code is working
 let map = L.map('mapid').setView([34.0522, -118.2437],4);
 
-L.circleMarker([34.0522,-118.2437],{
-    radius:300,
-    color:"black",
-    fillColor: '#ffffa1'
-}).addTo(map);
 // create tile layer that is the background of the map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,4 +10,18 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
+//Get data from cities.js
+let cityData = cities;
+
+cityData.forEach(function(city){
+    console.log(city);
+    L.circleMarker(city.location,{
+        radius: (city.population-200000)/100000,
+        color: "orange",
+        fillColor: "#ffffa1",
+        linewidth: 4
+        
+    })
+    .bindPopup("<h2>"+ city.city +", "+ city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() +"</h3>")
+    .addTo(map);
+});
