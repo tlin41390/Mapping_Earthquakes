694fdf082f82035c694a309d2f6160ceefcf6574
unknown <tlin41@dons.usfca.edu>
2021-02-27 11:03:47 -0800

    created geoJSON marker.

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,5 +1,5 @@
 //Add console.log to check to see if our code is working
-let map = L.map('mapid').setView([37.6213, -122.3790],5);
+let map = L.map('mapid').setView([37.5, -122.5],10);
 
 // create tile layer that is the background of the map.
 // We create the tile layer that will be the background of our map.
@@ -10,25 +10,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-//Get data from cities.js
-
-let line = [
-    [33.9416, -118.4085],
-    [37.6213,-122.3790],
-    [40.7899,-111.9791],
-    [47.4502,-122.3088]
-];
-
-let airportRoute = [
-    [37.6215083120081, -122.37898758089858],
-    [30.197591590241906, -97.66626706206424],
-    [43.677810667728835, -79.62461584807939],
-    [40.64140060895507, -73.77807472388615],
-];
-L.polyline(airportRoute, {
-    strokeOpacity:0,
-    weight: '4',
-    dashArray: '10, 10',
-    dashOffset: '20'
-    
-}).addTo(map);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type": "FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city": "San Francisco",
+        "country":"United States",
+        "faa": "SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+        ]};
+//Grabbing the GeoJson data.
+L.geoJSON(sanFranAirport).addTo(map);
