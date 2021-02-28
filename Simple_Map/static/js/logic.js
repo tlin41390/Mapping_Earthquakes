9323127ab9fee2a29498fc472a683724209de3e1
unknown <tlin41@dons.usfca.edu>
2021-02-28 15:21:14 -0800

    created neighborhood polygon in Toronto.

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,42 +1,41 @@
 // create tile layer that is the background of the map.
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 let baseMaps = {
-    Street: light,
-    Dark: dark
+    Street: streets,
+    Satellite_Streets: satelliteStreets
 };
 
 let map = L.map('mapid',{
-    center: [44.0,-80.0],
+    center: [43.7,-79.3],
     zoom: 2,
-    layers: [light]   
+    layers: [satelliteStreets]   
 })
 L.control.layers(baseMaps).addTo(map);
 
 //Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/tlin41390/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Simple_Map/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/tlin41390/Mapping_Earthquakes/main/torontoNeighborhoods.json"
 //Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data){
+d3.json(torontoHoods).then(function(data){
     console.log(data);
     //Create GeoJSON layer with the retrieved data.
-    L.geoJson(data,{
-        color: "yellow",
-        weight: 2,
-        onEachFeature: function(feature, layer){
-            layer.bindPopup("<h3> Airline: "+ feature.properties.airline+ "</h3> <hr><h3> Destination: "
-            + feature.properties.dst +"</h3>");
+    L.geoJSON(data,{
+        fillColor:"yellow",
+        color:"yellow",
+        weight:1,
+        onEachFeature: function(feature,layer){
+            layer.bindPopup("<h3> Neighborhood: "+feature.properties.AREA_NAME+ "</h3>")
         }
-    })
-    .addTo(map);
+    }).addTo(map);
 });
